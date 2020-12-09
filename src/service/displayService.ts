7530b827062177454eece683784b7dcb5e8cddf3
Meng <Mengli619@github.com>
2020-12-09 19:15:15 +0800

    add shortcuts

--- src/service/displayService.ts
+++ src/service/displayService.ts
@@ -5,14 +5,14 @@ import { ipcRenderer } from 'electron';
 export class DisplayService {
 
   public createOBSDisplay(name: string, electronWindowId: number, scaleFactor: number, sourceId: string): void {
-    ipcRenderer.sendSync('createOBSDisplay', name, electronWindowId, scaleFactor, sourceId);
+    ipcRenderer.send('createOBSDisplay', name, electronWindowId, scaleFactor, sourceId);
   }
 
   public moveOBSDisplay(name: string, x: number, y: number, width: number, height: number): void {
-    ipcRenderer.sendSync('moveOBSDisplay', name, x, y, width, height);
+    ipcRenderer.send('moveOBSDisplay', name, x, y, width, height);
   }
 
   public destroyOBSDisplay(name: string): void {
-    ipcRenderer.sendSync('destroyOBSDisplay', name);
+    ipcRenderer.send('destroyOBSDisplay', name);
   }
 }
